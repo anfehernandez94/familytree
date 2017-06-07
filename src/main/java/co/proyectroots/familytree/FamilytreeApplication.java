56d5e529e13109dcf725c7bec76ac43658cc695e
anfehernandez94 <anfehernandez94@gmail.com>
2017-06-07 16:22:36 -0500

    first commit

---
+++ src/main/java/co/proyectroots/familytree/FamilytreeApplication.java
@@ -0,0 +1,12 @@
+package co.proyectroots.familytree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilytreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilytreeApplication.class, args);
+	}
+}
